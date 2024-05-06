17f70344f3e1d9d658835f4e80a22830a70b7a6f
Abiodun Lasisi <lasisiabioduntowobola@gmail.com>
2024-05-06 10:32:20 +0100

    access block updated

--- main.tf
+++ main.tf
@@ -14,7 +14,16 @@ resource "aws_s3_bucket" "game_bucket" {
 
 resource "aws_s3_bucket_acl" "example" {
   bucket = "my-meme-card-game"
-  acl    = "public-read"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.game_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
