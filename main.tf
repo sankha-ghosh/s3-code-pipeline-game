97f68213137e25a853ed5fd1e1791604e77cfadd
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-05-07 18:39:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,18 +40,18 @@ resource "aws_s3_bucket_policy" "host_bucket_policy" {
   bucket =  aws_s3_bucket.test-bucket.id # ID of the S3 bucket
 
   # Policy JSON for allowing public read access
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid": "PublicReadGetObject",
-        "Effect" : "Allow",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource": "arn:aws:s3:::${var.my_bucket_name}/*"
-      }
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*",
+                "s3-object-lambda:*"
+            ],
+            "Resource": "*"
+        }
     ]
-  })
 }
 
 
