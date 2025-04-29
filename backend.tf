4fd1b5451290e00437bebbdd241b1927bcd35170
sree12345789 <51334685+sree12345789@users.noreply.github.com>
2025-04-29 14:26:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,11 +2,11 @@
     terraform {
       backend "remote" {
         # The name of your Terraform Cloud organization.
-        organization = "Cloud-Platform"
+        organization = "cicd-testing"
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-          name = "abbey-terraform-aws"
+          name = "dev-app-infra"
         }
       }
     }
